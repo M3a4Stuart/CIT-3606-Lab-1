b7bd648ae2e04625d875338dd9d055b20fe7393a
M3a4Stuart <162371388+M3a4Stuart@users.noreply.github.com>
2025-09-24 11:08:41 -0400

    lab 3

--- Lab 3 Currency Converter UI/script.js
+++ Lab 3 Currency Converter UI/script.js
@@ -2,18 +2,4 @@ tofixed(2);
           alert(`${amount} ${fromCurrency} is approximately ${convertedAmount} ${toCurrency}`);
 
 
-      document.getElementById("convertButton").value = function() {
-        const amount = document.getElementById("amount").value;
-        const fromCurrency = document.getElementById("fromCurrency").value;
-        const toCurrency = document.getElementById("toCurrency").value;
-        const convertedAmount = (amount * exchangeRates[fromCurrency][toCurrency]).toFixed();2;
-        alert(`${amount} ${fromCurrency} is approximately ${convertedAmount} ${toCurrency}`);};2;
-
-      document.getElementById("convertButton").onclick = function() {
-        const amount = document.getElementById("amount").value;
-        const fromCurrency = document.getElementById("fromCurrency").value;
-        const toCurrency = document.getElementById("toCurrency").value;
-        const convertedAmount = (amount * exchangeRates[fromCurrency][toCurrency]).toFixed(2);
-        alert(`${amount} ${fromCurrency} is approximately ${convertedAmount} ${toCurrency}`);};2;
-      
       
