f6d15efe1e28ee60659f3ba30e5f966e4e334b5b
M3a4Stuart <162371388+M3a4Stuart@users.noreply.github.com>
2025-09-24 11:05:49 -0400

    lab 3

--- Lab 3 Currency Converter UI/script.js
+++ Lab 3 Currency Converter UI/script.js
@@ -16,24 +16,4 @@ tofixed(2);
         const convertedAmount = (amount * exchangeRates[fromCurrency][toCurrency]).toFixed(2);
         alert(`${amount} ${fromCurrency} is approximately ${convertedAmount} ${toCurrency}`);};2;
       
-      <><if value euro>
-  <tofixed />(2);
-  let num = 0.85;
-  let n = num.toFixed(2);
-  alert(n);
-</if><if value usd>
-    <tofixed />(2);
-    let num = 1.00;
-    let n = num.toFixed(2);
-    alert(n);
-  </if><if value gbp>
-    <tofixed />(2);
-    let num = 0.75;
-    let n = num.toFixed(2);
-    alert(n);
-  </if><if value jpy>
-    <tofixed />(2);
-    let num = 110.00;
-    let n = num.toFixed(2);
-    alert(n);
-  </if></>
+      
