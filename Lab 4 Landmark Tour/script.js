9fb8ae47f144eac755073c626abdd0b6fd3f42cc
M3a4Stuart <162371388+M3a4Stuart@users.noreply.github.com>
2025-09-29 11:25:04 -0400

    lab

--- Lab 4 Landmark Tour/script.js
+++ Lab 4 Landmark Tour/script.js
@@ -1,3 +1,7 @@
-document.getElementById('yourElementId').onclick = function() {
-	
+document.getElementById('Ukraine').onclick = function() {
+	img=document.getElementById('Landmarkimg');
+    img.src="gorod-pripyat-chernobyl.webp";
 };
+
+document.getElementById('Sweeden').onclick= function() {
+    img=doccument.getElementById('landmarkimg')
