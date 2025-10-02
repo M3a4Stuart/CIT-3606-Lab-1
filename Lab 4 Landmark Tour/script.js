08021b9cfee9383bab0a6342a10bcd9a65087855
M3a4Stuart <162371388+M3a4Stuart@users.noreply.github.com>
2025-10-02 09:34:16 -0400

    Update script.js

--- Lab 4 Landmark Tour/script.js
+++ Lab 4 Landmark Tour/script.js
@@ -1,17 +1,17 @@
 document.getElementById('Ukraine').onclick = function() {
 	img=document.getElementById('Landmarkimg');
     img.src="gorod-pripyat-chernobyl.webp";
-};
+}
 
 document.getElementById('Sweeden').onclick= function() {
     changeimg=document.getElementById('landmarkimg')
     img.src="Icehotel_entre_ms.jpg";
-};
+}
 
 document.getElementById('Warzaw').onclick= function() {
     changeimg=document.getElementById('landmarkimg')
     img.src="Sztuczna_palma_w_Warszawie_2025.jpg";
-};
+}
 
 document.getElementById(imgsize).value=onchange=function(){
     var value=document.getElementById("imgsize").value;
