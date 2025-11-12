334ee1b0216cb12492c1979ec524fe01d9acb9c9
M3a4Stuart <162371388+M3a4Stuart@users.noreply.github.com>
2025-11-12 11:27:32 -0500

    lab 8

--- Lab 7 React Pokedex/react-demo/src/index.js
+++ Lab 7 React Pokedex/react-demo/src/index.js
@@ -11,6 +11,14 @@ root.render(
   </React.StrictMode>
 );
 
+const express = require('express'); 
+const app = express();
+app.get('/', function(req, res){
+   res.send("Hello world!");
+});
+app.listen(8080);
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
