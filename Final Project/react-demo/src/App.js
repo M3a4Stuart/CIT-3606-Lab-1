13879e14432368515040944aeb8e61fdc25bef84
M3a4Stuart <162371388+M3a4Stuart@users.noreply.github.com>
2025-12-01 11:37:12 -0500

    Final

---
+++ Final Project/react-demo/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Power Armor index</h1>
+      </header>
+    </div>  
+    <div className="armor-list">
+      <div className="armor-card">
+        <h2>T-45 Power Armor</h2>
+  );
+}
+
+export default App;
