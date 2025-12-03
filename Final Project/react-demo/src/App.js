d10401193aa1e7c5afb403d8cdd1fa57406c2d30
M3a4Stuart <162371388+M3a4Stuart@users.noreply.github.com>
2025-12-03 13:13:03 -0500

    lab final

--- Final Project/react-demo/src/App.js
+++ Final Project/react-demo/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src= 'images/logo.jpg' alt='logo' />
         <h1>Power Armor index</h1>
       </header>
 
@@ -20,6 +20,7 @@ function App() {
         <p>Total Durability: 450</p>
         <p>weight: 92</p>
         <p>Repair Cost: Steel: 27, Circuitry: 1</p>
+        <p>Unique Variants: Piezonuccleic power armor (torso)</p>
       </div>
       <div className="armor-list">
         <h2>T-51 Power Armor</h2>
@@ -29,7 +30,8 @@ function App() {
         <p>Radiation Resitance: 1050</p>
         <p>Total Durability: 720</p>
         <p>weight: 92</p>
-        <p>Repair Cost: Steel: 33, Aluminum: 1, Circuitry: 1</p>        
+        <p>Repair Cost: Steel: 33, Aluminum: 1, Circuitry: 1</p> 
+        <p>Unique Variants: Piezonuccleic power armor (torso)</p>       
         </div>
       <div className="armor-list">
         <h2>T-60 Power Armor</h2>
@@ -40,6 +42,7 @@ function App() {
         <p>Total Durability: 990</p>
         <p>weight: 92</p>
         <p>Repair Cost: Steel: 40, Plastic: 8, Aluminum: 8, Circuitry: 1</p>
+        <p>Unique Variants: Tesla T-60(left-right arms), Visonary's To60c (helmet), Exemplar's T-60c (torso), Honor (left leg), Vengence (right leg)</p>
       </div>
       <div className="armor-list">
         <h2>T-65 Power Armor</h2>
@@ -51,6 +54,7 @@ function App() {
         <p>weight: 120</p>
         <p>Repair Cost: Aluminum: 90, Black Titanium: 60, CircitryL: 14, Gears: 45 Glass: 17, Nuclear Material: 56, Oil
           :80, Plastic: 40 Rubber: 90, Screws: 84, Silver: 30, Steel: 156</p>
+        <p>Unique Variants: N/A</p>
       </div>
       <div className="armor-list">
         <h2>X-01 Advanced Power Armor</h2>
@@ -61,6 +65,7 @@ function App() {
       <p>Total Durability: 1250</p>
       <p>weight: 92</p>
       <p>Repair Cost: Steel: 50, Aluminum: 10, Circuitry: 5, Nuclear Material: 2</p>
+      <p>Unique Variants: Quantum X-01 power armor</p>
       </div>
       <div classname ="armor-list"> 
         <h2>X-02 Advanced Power Armor</h2>
@@ -71,6 +76,7 @@ function App() {
       <p>Total Durability: 1600</p>
       <p>weight: 110</p>
       <p>Repair Cost: Steel: 60, Aluminum: 15, Circuitry: 8, Nuclear Material: 5</p>
+      <p>Unique Variants: Devil's Inferno (torso), Tesla Coil (full suit) </p>
       </div>
       <div classname ="armor-list">
         <h2>X-03 Advanced Hellfire Power Armor</h2>
@@ -81,6 +87,7 @@ function App() {
       <p>Total Durability: 2000</p>
       <p>weight: 130</p>
       <p>Repair Cost: Steel: 75, Aluminum: 20, Circuitry: 10, Nuclear Material: 8, Ceramic: 1</p>
+      <p>Unique Variants: N/A</p>
       </div>
       <div classname ="armor-list">
         <h2>Vulcan Power Armor</h2>
@@ -91,6 +98,7 @@ function App() {
       <p>Total Durability: 6000</p>
       <p>weight: 500</p>
       <p>Repair Cost: Steel: 100, Aluminum: 30, Circuitry: 15, Nuclear Material: 10, Ceramic: 5, Black Titanium: 10</p>
+      <p>Unique Variants: N/A</p>
       </div>
       <div classname ="armor-list"> 
         <h2>Ultracite Power Armor</h2>
@@ -101,6 +109,7 @@ function App() {
       <p>Total Durability: 3000</p>
       <p>weight: 180</p>
       <p>Repair Cost: Ultracite: 50, Steel: 150, Aluminum: 40, Circuitry: 20, Nuclear Material: 15, Ceramic: 10, Black Titanium: 20</p>
+      <p>Unique Variants: Strangler heart power armor</p>
       </div>
       <div classname ="armor-list">
         <h2>Union Power Armor</h2>
@@ -111,6 +120,7 @@ function App() {
       <p>Total Durability: 295</p>
       <p>weight: 85</p>
       <p>Repair Cost: Steel: 25, Aluminum: 5, Circuitry: 1</p>
+      <p>Unique Variants: N/A</p>
       </div>
       <div classname ="armor-list">
         <h2>Excavator Power Armor</h2>
@@ -121,6 +131,7 @@ function App() {
       <p>Total Durability: 240</p>
       <p>weight: 71</p>
       <p>Repair Cost: Steel: 20, Aluminum: 3, Circuitry: 1</p>
+      <p>Unique Variants: N/A</p>
     </div>
     <div classname ="armor-list">
       <h2>Hellcat Power Armor</h2>
@@ -131,6 +142,7 @@ function App() {
       <p>Total Durability: 320</p>
       <p>weight: 90</p>
       <p>Repair Cost: Steel: 28, Aluminum: 6, Circuitry: 1</p>
+      <p>Unique Variants: N/A</p>
     </div>
     <div classname = "armor-list">
       <h2>Raider Power Armor</h2>
@@ -140,7 +152,8 @@ function App() {
       <p>Radiation Resitance: 150</p>
       <p>Total Durability: 150</p>
       <p>weight: 60</p>
-      <p>Repair Cost: Steel: 15, Aluminum: 2, Circuitry: 1</p>  
+      <p>Repair Cost: Steel: 15, Aluminum: 2, Circuitry: 1</p>
+      <p>Unique Variants: Tessa's Fist (right arm), Overboss power armor, Raider II power armor</p>
     </div>
     </div>
   );
