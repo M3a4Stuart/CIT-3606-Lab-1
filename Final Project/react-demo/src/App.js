2658dc5ba7e773251ffb1637910936ba20363993
M3a4Stuart <162371388+M3a4Stuart@users.noreply.github.com>
2025-12-03 12:55:58 -0500

    Update App.js

--- Final Project/react-demo/src/App.js
+++ Final Project/react-demo/src/App.js
@@ -26,9 +26,9 @@ function App() {
       <img src="images/T51B PA.jpg" alt="t51" />
         <p>Damage Resitance: 740</p>
         <p>Energy Resitance: 490</p>
-        <p>Radiation Resitance: 1300</p>
+        <p>Radiation Resitance: 1050</p>
         <p>Total Durability: 720</p>
-        <p>weight: 102</p>
+        <p>weight: 92</p>
         <p>Repair Cost: Steel: 33, Aluminum: 1, Circuitry: 1</p>        
         </div>
       <div className="armor-list">
@@ -36,23 +36,105 @@ function App() {
       <img src="images/T60 PA.jpg" alt="t60" />
         <p>Damage Resitance: 980</p>
         <p>Energy Resitance: 650</p>
-        <p>Radiation Resitance: 1250</p>
-        <p>Total Durability: 900</p>
-        <p>weight: 110</p>
+        <p>Radiation Resitance: 1050</p>
+        <p>Total Durability: 990</p>
+        <p>weight: 92</p>
         <p>Repair Cost: Steel: 40, Plastic: 8, Aluminum: 8, Circuitry: 1</p>
       </div>
       <div className="armor-list">
         <h2>T-65 Power Armor</h2>
       <img src="images/T65 PA.jpg" alt="t65" />
-        <p>Damage Resitance: 1200</p>
-        <p>Energy Resitance: 800</p>
-        <p>Radiation Resitance: 1500</p>
-        <p>Total Durability: 1100</p>
+        <p>Damage Resitance: 565</p>
+        <p>Energy Resitance: 470</p>
+        <p>Radiation Resitance: 470</p>
+        <p>Total Durability: 470</p>
         <p>weight: 120</p>
         <p>Repair Cost: Aluminum: 90, Black Titanium: 60, CircitryL: 14, Gears: 45 Glass: 17, Nuclear Material: 56, Oil
           :80, Plastic: 40 Rubber: 90, Screws: 84, Silver: 30, Steel: 156</p>
-        </p>
       </div>
+      <div className="armor-list">
+        <h2>X-01 Advanced Power Armor</h2>
+      <img src="images/X01 PA.jpg" alt="x01" />
+      <p>Damage Resitance: 1220</p>
+      <p>Energy Resitance: 790</p>
+      <p>Radiation Resitance: 1050</p>
+      <p>Total Durability: 1250</p>
+      <p>weight: 92</p>
+      <p>Repair Cost: Steel: 50, Aluminum: 10, Circuitry: 5, Nuclear Material: 2</p>
+      </div>
+      <div classname ="armor-list"> 
+        <h2>X-02 Advanced Power Armor</h2>
+      <img src="images/X02 PA.jpg" alt="x02" />
+      <p>Damage Resitance: 1500</p>
+      <p>Energy Resitance: 950</p>
+      <p>Radiation Resitance: 1200</p>
+      <p>Total Durability: 1600</p>
+      <p>weight: 110</p>
+      <p>Repair Cost: Steel: 60, Aluminum: 15, Circuitry: 8, Nuclear Material: 5</p>
+      </div>
+      <div classname ="armor-list">
+        <h2>X-03 Advanced Hellfire Power Armor</h2>
+      <img src="images/X03 PA.jpg" alt="x03" />
+      <p>Damage Resitance: 1800</p>
+      <p>Energy Resitance: 1200</p>
+      <p>Radiation Resitance: 1500</p>
+      <p>Total Durability: 2000</p>
+      <p>weight: 130</p>
+      <p>Repair Cost: Steel: 75, Aluminum: 20, Circuitry: 10, Nuclear Material: 8, Ceramic: 1</p>
+      </div>
+      <div classname ="armor-list">
+        <h2>Vulcan Power Armor</h2>
+      <img src="images/Vulcan PA.jpg" alt="vulcan" />
+      <p>Damage Resitance: 560</p>
+      <p>Energy Resitance: 600</p>
+      <p>Radiation Resitance: 200</p>
+      <p>Total Durability: 6000</p>
+      <p>weight: 500</p>
+      <p>Repair Cost: Steel: 100, Aluminum: 30, Circuitry: 15, Nuclear Material: 10, Ceramic: 5, Black Titanium: 10</p>
+      </div>
+      <div classname ="armor-list"> 
+        <h2>Ultracite Power Armor</h2>
+      <img src="images/Ultracite PA.jpg" alt="ultracite" />
+      <p>Damage Resitance: 2500</p>
+      <p>Energy Resitance: 2000</p>
+      <p>Radiation Resitance: 2200</p>
+      <p>Total Durability: 3000</p>
+      <p>weight: 180</p>
+      <p>Repair Cost: Ultracite: 50, Steel: 150, Aluminum: 40, Circuitry: 20, Nuclear Material: 15, Ceramic: 10, Black Titanium: 20</p>
+      </div>
+      <div classname ="armor-list">
+        <h2>Union Power Armor</h2>
+      <img src="images/Union PA.jpg" alt="union" />
+      <p>Damage Resitance: 483</p>
+      <p>Energy Resitance: 295</p>
+      <p>Radiation Resitance: 265</p>
+      <p>Total Durability: 295</p>
+      <p>weight: 85</p>
+      <p>Repair Cost: Steel: 25, Aluminum: 5, Circuitry: 1</p>
+      </div>
+      <div classname ="armor-list">
+        <h2>Excavator Power Armor</h2>
+      <img src="images/Excavator PA.jpg" alt="excavator" />
+      <p>Damage Resitance: 240</p>
+      <p>Energy Resitance: 240</p>
+      <p>Radiation Resitance: 366</p>
+      <p>Total Durability: 240</p>
+      <p>weight: 71</p>
+      <p>Repair Cost: Steel: 20, Aluminum: 3, Circuitry: 1</p>
+    </div>
+    <div classname ="armor-list">
+      <h2>Hellcat Power Armor</h2>
+      <img src="images/Hellcat PA.jpg" alt="hellcat" />
+      <p>Damage Resitance: 536</p>
+      <p>Energy Resistance: 320</p>
+      <p>Radiation Resistance: 320</p>
+      <p>Total Durability: 320</p>
+      <p>weight: 90</p>
+      <p>Repair Cost: Steel: 28, Aluminum: 6, Circuitry: 1</p>
+    </div>
+    <div classname = "armor-list">
+      
+    </div>
     </div>
   );
 }
