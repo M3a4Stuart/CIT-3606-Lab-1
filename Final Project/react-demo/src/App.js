c9a54a1190a9195a2e96d81fec0bd810134bd602
M3a4Stuart <162371388+M3a4Stuart@users.noreply.github.com>
2025-12-04 12:38:00 -0500

    Update App.js

--- Final Project/react-demo/src/App.js
+++ Final Project/react-demo/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 
@@ -11,7 +10,7 @@ function App() {
         <h1>Power Armor index</h1>
       </header>
 
-      <div className="armor-list">
+      <div className="armorList">
         <h2>T-45 Power Armor</h2>
         <img src="images/T45A PA.jpg" alt="t45" />
         <p>Damge Resitance: 500</p>
@@ -21,8 +20,10 @@ function App() {
         <p>weight: 92</p>
         <p>Repair Cost: Steel: 27, Circuitry: 1</p>
         <p>Unique Variants: Piezonuccleic power armor (torso)</p>
+        <p>Games Aquired: Fallout 4, Fallout 76</p>
       </div>
-      <div className="armor-list">
+      
+      <div className="armorList">
         <h2>T-51 Power Armor</h2>
       <img src="images/T51B PA.jpg" alt="t51" />
         <p>Damage Resitance: 740</p>
@@ -31,9 +32,11 @@ function App() {
         <p>Total Durability: 720</p>
         <p>weight: 92</p>
         <p>Repair Cost: Steel: 33, Aluminum: 1, Circuitry: 1</p> 
-        <p>Unique Variants: Piezonuccleic power armor (torso)</p>       
+        <p>Unique Variants: Piezonuccleic power armor (torso)</p>
+        <p>Games Aquired: Fallout 4, Fallout 76</p>    
         </div>
-      <div className="armor-list">
+
+      <div className="armorList">
         <h2>T-60 Power Armor</h2>
       <img src="images/T60 PA.jpg" alt="t60" />
         <p>Damage Resitance: 980</p>
@@ -43,8 +46,10 @@ function App() {
         <p>weight: 92</p>
         <p>Repair Cost: Steel: 40, Plastic: 8, Aluminum: 8, Circuitry: 1</p>
         <p>Unique Variants: Tesla T-60(left-right arms), Visonary's To60c (helmet), Exemplar's T-60c (torso), Honor (left leg), Vengence (right leg)</p>
+        <p>Games Aquired: Fallout 4, Fallout 76</p>
       </div>
-      <div className="armor-list">
+
+      <div className="armorList">
         <h2>T-65 Power Armor</h2>
       <img src="images/T65 PA.jpg" alt="t65" />
         <p>Damage Resitance: 565</p>
@@ -55,8 +60,10 @@ function App() {
         <p>Repair Cost: Aluminum: 90, Black Titanium: 60, CircitryL: 14, Gears: 45 Glass: 17, Nuclear Material: 56, Oil
           :80, Plastic: 40 Rubber: 90, Screws: 84, Silver: 30, Steel: 156</p>
         <p>Unique Variants: N/A</p>
+        <p>Games Aquired: Fallout 76</p>
       </div>
-      <div className="armor-list">
+      
+      <div className="armorList">
         <h2>X-01 Advanced Power Armor</h2>
       <img src="images/X01 PA.jpg" alt="x01" />
       <p>Damage Resitance: 1220</p>
@@ -66,8 +73,10 @@ function App() {
       <p>weight: 92</p>
       <p>Repair Cost: Steel: 50, Aluminum: 10, Circuitry: 5, Nuclear Material: 2</p>
       <p>Unique Variants: Quantum X-01 power armor</p>
+      <p>Games Aquired: Fallout 4, Fallout 76</p>
       </div>
-      <div classname ="armor-list"> 
+
+      <div classname ="armorList"> 
         <h2>X-02 Advanced Power Armor</h2>
       <img src="images/X02 PA.jpg" alt="x02" />
       <p>Damage Resitance: 1500</p>
@@ -77,8 +86,10 @@ function App() {
       <p>weight: 110</p>
       <p>Repair Cost: Steel: 60, Aluminum: 15, Circuitry: 8, Nuclear Material: 5</p>
       <p>Unique Variants: Devil's Inferno (torso), Tesla Coil (full suit) </p>
+      <p>Games Aquired: Fallout 4, Fallout 76</p>
       </div>
-      <div classname ="armor-list">
+      
+      <div classname ="armorList">
         <h2>X-03 Advanced Hellfire Power Armor</h2>
       <img src="images/X03 PA.jpg" alt="x03" />
       <p>Damage Resitance: 1800</p>
@@ -88,8 +99,10 @@ function App() {
       <p>weight: 130</p>
       <p>Repair Cost: Steel: 75, Aluminum: 20, Circuitry: 10, Nuclear Material: 8, Ceramic: 1</p>
       <p>Unique Variants: N/A</p>
+      <p>Games Aquired: Fallout 4, Fallout 76</p>
       </div>
-      <div classname ="armor-list">
+      
+      <div classname ="armorList">
         <h2>Vulcan Power Armor</h2>
       <img src="images/Vulcan PA.jpg" alt="vulcan" />
       <p>Damage Resitance: 560</p>
@@ -99,8 +112,10 @@ function App() {
       <p>weight: 500</p>
       <p>Repair Cost: Steel: 100, Aluminum: 30, Circuitry: 15, Nuclear Material: 10, Ceramic: 5, Black Titanium: 10</p>
       <p>Unique Variants: N/A</p>
+      <p>Games Aquired: Fallout 76</p>
       </div>
-      <div classname ="armor-list"> 
+
+      <div classname ="armorList"> 
         <h2>Ultracite Power Armor</h2>
       <img src="images/Ultracite PA.jpg" alt="ultracite" />
       <p>Damage Resitance: 2500</p>
@@ -110,8 +125,10 @@ function App() {
       <p>weight: 180</p>
       <p>Repair Cost: Ultracite: 50, Steel: 150, Aluminum: 40, Circuitry: 20, Nuclear Material: 15, Ceramic: 10, Black Titanium: 20</p>
       <p>Unique Variants: Strangler heart power armor</p>
+      <p>Games Aquired: Fallout 76</p>
       </div>
-      <div classname ="armor-list">
+
+      <div classname ="armorList">
         <h2>Union Power Armor</h2>
       <img src="images/Union PA.jpg" alt="union" />
       <p>Damage Resitance: 483</p>
@@ -121,8 +138,10 @@ function App() {
       <p>weight: 85</p>
       <p>Repair Cost: Steel: 25, Aluminum: 5, Circuitry: 1</p>
       <p>Unique Variants: N/A</p>
+      <p>Games Aquired: Fallout 76</p>
       </div>
-      <div classname ="armor-list">
+
+      <div classname ="armorList">
         <h2>Excavator Power Armor</h2>
       <img src="images/Excavator PA.jpg" alt="excavator" />
       <p>Damage Resitance: 240</p>
@@ -132,8 +151,10 @@ function App() {
       <p>weight: 71</p>
       <p>Repair Cost: Steel: 20, Aluminum: 3, Circuitry: 1</p>
       <p>Unique Variants: N/A</p>
+      <p>Games Aquired: Fallout 76</p>
     </div>
-    <div classname ="armor-list">
+
+    <div classname ="armorList">
       <h2>Hellcat Power Armor</h2>
       <img src="images/Hellcat PA.jpg" alt="hellcat" />
       <p>Damage Resitance: 536</p>
@@ -143,8 +164,10 @@ function App() {
       <p>weight: 90</p>
       <p>Repair Cost: Steel: 28, Aluminum: 6, Circuitry: 1</p>
       <p>Unique Variants: N/A</p>
+      <p>Games Aquired: Fallout 76</p>
     </div>
-    <div classname = "armor-list">
+
+    <div classname = "armorList">
       <h2>Raider Power Armor</h2>
       <img src="images/Raider PA.jpg" alt="raider" />
       <p>Damage Resitance: 150</p>
@@ -154,7 +177,9 @@ function App() {
       <p>weight: 60</p>
       <p>Repair Cost: Steel: 15, Aluminum: 2, Circuitry: 1</p>
       <p>Unique Variants: Tessa's Fist (right arm), Overboss power armor, Raider II power armor</p>
+      <p>Games Aquired: Fallout 4, Fallout 76</p>
     </div>
+
     </div>
   );
 }
